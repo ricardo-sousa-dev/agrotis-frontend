f689d72b1b9ffd32f342477acde4e00a40abd6f8
ricardo-sousa <rwmsousa@gmail.com>
2022-05-19 18:18:56 -0300

    feat: create test write in the form

---
+++ cypress/integration/1-form/form.test.js
@@ -0,0 +1,23 @@
+describe('Testa página do formulário de cadastro', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Verifica se a página possui o título correto', () => {
+    cy.get('h1').should('contain', 'Cadastro')
+  })
+
+  it('Deve preencher o formulário corretamente', () => {
+    cy.get('[data-cy=input-nome]').type('João da Silva')
+    cy.get('[data-cy=input-dataInicial]').type('22102020')
+    cy.get('[data-cy=input-dataFinal]').type('12122021')
+    cy.get('[data-cy=input-idPropriedade]').type('12345')
+    cy.get('[data-cy=input-nomePropriedade]').type('Fazenda Paraíso')
+    cy.get('[data-cy=input-cnpj]').type('27505101010101')
+    cy.get('[data-cy=input-idLaboratorio]').type('1234')
+    cy.get('[data-cy=input-nomeLaboratorio]').type('Laboratório de Testes')
+    cy.get('[data-cy=input-observacoes]').type('Observações')
+    cy.get('[data-cy=button-enviar]').should('to.have.length', 1)
+  })
+})
--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../css/pages/FormPage.css';
 import { useForm } from 'react-hook-form';
-import {cnpjMask} from '../utils/cnpjMask';
+import { cnpjMask, dateMask } from '../utils/inputMask';
 
 function FormPage() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -9,8 +9,8 @@ function FormPage() {
   const onSubmit = data => console.log(
     {
       nome: data.nome,
-      dataInicial: data.dataInicial,
-      dataFinal: data.dataFinal,
+      dataInicial: new Date(data.dataInicial).toISOString(),
+      dataFinal: new Date(data.dataFinal).toISOString(),
       infosPropriedade: {
         id: data.idProperty,
         nome: data.nomeProperty,
@@ -40,60 +40,83 @@ function FormPage() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="nome">
           <label>Nome</label>
-          <input {...register("nome", { required: true })} type='text' />
+          <input {...register("nome", { required: true })}
+            type='text'
+            data-cy="input-nome" />
           {errors.nome?.type === 'required' && "Preenchimento do Nome é obrigatório"}
         </div>
 
         <div className="dataInicial" >
           <label>Data Inicial</label>
-          <input {...register("dataInicial", { required: true })} type='date' />
+          <input {...register("dataInicial", { required: true })}
+            data-cy="input-dataInicial"
+            type="text"
+            placeholder="dd/mm/aaaa"
+            name='dataInicial' />
           {errors.dataInicial?.type === 'required' && "Preenchimento da Data Inicial é obrigatório"}
         </div>
 
         <div className="dataFinal" >
           <label>Data Final</label>
-          <input {...register("dataFinal", { required: true })} type='date' />
+          <input {...register("dataFinal", { required: true })}
+            data-cy="input-dataFinal"
+            type="text"
+            placeholder="dd/mm/aaaa"
+            name='dataFinal' />
           {errors.dataFinal?.type === 'required' && "Preenchimento da Data Final é obrigatório"}
         </div>
 
-        <div className="idProperty" >
+        <div className="idPropriedade" >
           <label>Id da Propriedade</label>
-          <input {...register("idProperty", { required: true })} type='number' />
+          <input {...register("idPropriedade", { required: true })}
+            type='number'
+            data-cy='input-idPropriedade' />
           {errors.idProperty?.type === 'required' && "Preenchimento do Id da Propriedade é obrigatório"}
         </div>
 
-        <div className="nameProperty">
+        <div className="nomePropriedade">
           <label>Nome da Propriedade</label>
-          <input {...register("nomeProperty", { required: true })} type='text' />
+          <input {...register("nomePropriedade", { required: true })}
+            type='text'
+            data-cy='input-nomePropriedade' />
           {errors.nomeProperty?.type === 'required' && "Preenchimento do Nome da Propriedade é obrigatório"}
         </div>
 
         <div className="cnpj" >
           <label>CNPJ da Propriedade</label>
-          <input {...register("cnpj", { required: true })} name='cnpj'
+          <input {...register("cnpj", { required: true })}
+            name='cnpj'
+            data-cy="input-cnpj"
             value={cnpjMask(values.cnpj)}
-            onChange={inputChange}/>
+            onChange={inputChange} />
           {errors.cnpj?.type === 'required' && "Preenchimento do CNPJ da Propriedade é obrigatório"}
         </div>
 
         <div className="idLaboratorio">
           <label>Id do Laboratório</label>
-          <input {...register("idLaboratorio", { required: true })} type='number' />
+          <input {...register("idLaboratorio", { required: true })}
+            type='number'
+            data-cy="input-idLaboratorio" />
           {errors.idLaboratorio?.type === 'required' && "Preenchimento do Id do Laboratório é obrigatório"}
         </div>
 
         <div className="nomeLaboratorio">
           <label>Nome do Laboratório</label>
-          <input {...register("nomeLaboratorio", { required: true })} type='text' />
+          <input {...register("nomeLaboratorio", { required: true })}
+            type='text'
+            data-cy="input-nomeLaboratorio" />
           {errors.nomeLaboratorio?.type === 'required' && "Preenchimento do Nome do Laboratório é obrigatório"}
         </div>
 
         <div className="observacoes">
           <label>Observações</label>
-          <textarea {...register("observacoes")} />
+          <textarea {...register("observacoes")}
+            data-cy="input-observacoes"
+          />
         </div>
 
-        <input type="submit" />
+        <input type="submit" 
+        data-cy="button-enviar"/>
       </form>
     </>
   );
