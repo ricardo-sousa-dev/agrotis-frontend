b8f85dffb3495a0ed24a45aba5824bfc83b78698
ricardo-sousa <rwmsousa@gmail.com>
2022-05-19 11:52:40 -0300

    feat: create validations and format data

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -4,61 +4,77 @@ import { useForm } from 'react-hook-form';
 
 function FormPage() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = data => console.log(
+    {
+      nome: data.nome,
+      dataInicial: data.dataInicial,
+      dataFinal: data.dataFinal,
+      infosPropriedade: {
+        id: data.idProperty,
+        nome: data.nomeProperty,
+      },
+      cnpj: data.cnpj,
+      laboratorio: {
+        id: data.idLaboratorio,
+        nome: data.nomeLaboratorio,
+      },
+      observacoes: data.observacoes,
+    }
+  );
 
   return (
     <>
       <h1>Cadastro</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="name">
+        <div className="nome">
           <label>Nome</label>
-          <input {...register("name", { required: true })} />
-          {errors.name?.type === 'required' && "Preenchimento do Nome é obrigatório"}
+          <input {...register("nome", { required: true })} />
+          {errors.nome?.type === 'required' && "Preenchimento do Nome é obrigatório"}
         </div>
 
-        <div className="initDate" >
+        <div className="dataInicial" >
           <label>Data Inicial</label>
-          <input {...register("dataInicial")} />
+          <input {...register("dataInicial", { required: true })} />
           {errors.dataInicial?.type === 'required' && "Preenchimento da Data Inicial é obrigatório"}
         </div>
 
-        <div className="endDate" >
+        <div className="dataFinal" >
           <label>Data Final</label>
-          <input {...register("dataFinal")} />
+          <input {...register("dataFinal", { required: true })} />
           {errors.dataFinal?.type === 'required' && "Preenchimento da Data Final é obrigatório"}
         </div>
 
         <div className="idProperty" >
           <label>Id da Propriedade</label>
-          <input {...register("idProperty")} />
+          <input {...register("idProperty", { required: true })} />
           {errors.idProperty?.type === 'required' && "Preenchimento do Id da Propriedade é obrigatório"}
         </div>
 
         <div className="nameProperty">
           <label>Nome da Propriedade</label>
-          <input {...register("nameProperty")} />
-          {errors.nameProperty?.type === 'required' && "Preenchimento do Nome da Propriedade é obrigatório"}
+          <input {...register("nomeProperty", { required: true })} />
+          {errors.nomeProperty?.type === 'required' && "Preenchimento do Nome da Propriedade é obrigatório"}
         </div>
 
-        <div className="cnpjProperty" >
+        <div className="cnpj" >
           <label>CNPJ da Propriedade</label>
-          <input {...register("cnpj")} />
+          <input {...register("cnpj", { required: true })} />
           {errors.cnpj?.type === 'required' && "Preenchimento do CNPJ da Propriedade é obrigatório"}
         </div>
 
-        <div className="idLab">
+        <div className="idLaboratorio">
           <label>Id do Laboratório</label>
-          <input {...register("idLaboratorio")} />
+          <input {...register("idLaboratorio", { required: true })} />
           {errors.idLaboratorio?.type === 'required' && "Preenchimento do Id do Laboratório é obrigatório"}
         </div>
 
-        <div className="nameLab">
+        <div className="nomeLaboratorio">
           <label>Nome do Laboratório</label>
-          <input {...register("nomeLaboratorio")} />
-          {errors.nameLaboratorio?.type === 'required' && "Preenchimento do Nome do Laboratório é obrigatório"}
+          <input {...register("nomeLaboratorio", { required: true })} />
+          {errors.nomeLaboratorio?.type === 'required' && "Preenchimento do Nome do Laboratório é obrigatório"}
         </div>
 
-        <div className="comments">
+        <div className="observacoes">
           <label>Observações</label>
           <textarea {...register("observacoes")} />
         </div>
