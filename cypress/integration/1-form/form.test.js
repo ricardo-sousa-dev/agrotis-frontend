f3c867d63d9507e5e38ed69add5cdbcac377347a
ricardo-sousa <rwmsousa@gmail.com>
2022-05-20 13:13:28 -0300

    test: implementation test in the mask fields

--- cypress/integration/1-form/form.test.js
+++ cypress/integration/1-form/form.test.js
@@ -8,17 +8,27 @@ describe('Testa página do formulário de cadastro', () => {
     cy.get('h1').should('contain', 'Cadastro')
   })
 
-  it('Deve preencher o formulário corretamente', () => {
+  it('Verifica se as máscaras de CNPJ e Data formatam os dados corretamente', () => {
+    cy.get('input[name="cnpj"]')
+      .type('12345678901234')
+      .should('have.value', '12.345.678/9012-34')
+
+    cy.get('input[name="dataInicial"]')
+      .type('12012020')
+      .should('have.value', '12/01/2020')
+  })
+
+  it('Verifica se os campos do formulário são preenchidos corretamente e são enviados', () => {
     cy.get('[data-cy=input-nome]').type('João da Silva')
-    cy.get('[data-cy=input-dataInicial]').click('right')
-    cy.get(".owl-calendar-year span:nth-child(15)").click();
-    // cy.get('[data-cy=input-dataFinal]').type('12122021')
+    cy.get('[data-cy=input-dataInicial]').type('22102000')
+    cy.get('[data-cy=input-dataFinal]').type('10122010')
     cy.get('[data-cy=input-idPropriedade]').type('12345')
     cy.get('[data-cy=input-nomePropriedade]').type('Fazenda Paraíso')
     cy.get('[data-cy=input-cnpj]').type('27505101010101')
     cy.get('[data-cy=input-idLaboratorio]').type('1234')
     cy.get('[data-cy=input-nomeLaboratorio]').type('Laboratório de Testes')
     cy.get('[data-cy=input-observacoes]').type('Observações')
-    cy.get('[data-cy=button-enviar]').should('to.have.length', 1)
+    cy.get('[data-cy=button-enviar]').click()
+    cy.contains('Enviando...')
   })
 })
